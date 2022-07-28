d9859d0e23fdc9d9bf171fc6d0f6a3aae9197917
Dima Rosmait <d.rosmait@datahow.ch>
2022-07-28 08:37:22 +0200

    Add cont colors

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,4 +1,5 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
+import Colors from "../constants/colors";
 
 function PrimaryButton({ children, style, onPress }) {
   return (
@@ -9,7 +10,7 @@ function PrimaryButton({ children, style, onPress }) {
           pressed && styles.pressed,
         ]}
         onPress={onPress}
-        android_ripple={{ color: "#640233" }}
+        android_ripple={{ color: Colors.primary600 }}
       >
         <Text style={styles.buttonText}>{children}</Text>
       </Pressable>
@@ -26,7 +27,7 @@ const styles = StyleSheet.create({
     overflow: "hidden",
   },
   buttonInnerContainer: {
-    backgroundColor: "#72063c",
+    backgroundColor: Colors.primary500,
     paddingVertical: 8,
     paddingHorizontal: 16,
     elevation: 2,
